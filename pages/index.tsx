cbd96f4ed585c2b6dfb97a24ed541d17432fe474
razznblue <kaipojames12@gmail.com>
2023-12-20 23:04:53 -0600

    Add Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
-import styles from '@/styles/Home.module.css'
-// import { useState, useEffect } from 'react'
-// import { useSession } from 'next-auth/react'
-// import axios from 'axios'
-// import Image from 'next/image'
+import Button from '@/components/button/Button'
+import { useState } from 'react'
+import { signIn, useSession } from 'next-auth/react'
+import { primaryColor } from '@/util/constants'
 
 export default function Home() {
-  // const { data: session } = useSession()
+  const { data: session } = useSession()
 
-  // useEffect(() => {
-  //   if (!session) {
-
-  //   }
-  // }, [])
+  /* Hooks */
+  const [username, setUsername] = useState('Kyle')
 
   /* JSX */
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Hello Kyle</h1>
-        <p>
-          "Being passive gets you nowhere. Being aggressive gets you noticed. Being assertive gets
-          you respect." - Steve Maraboli
-        </p>
+    <div className="flex items-center justify-center h-screen">
+      <main className="flex flex-col items-center justify-evenly h-full">
+        {!session ? (
+          <p className="text-white p-3 m-2 text-4xl text-center">
+            Please Sign In to start practicing!
+          </p>
+        ) : (
+          <>
+            <div id="top-content" className="flex flex-col items-center">
+              <h1 className={`text-[${primaryColor}] text-3xl`}>Hello {username}!</h1>
+              <p className="text-white p-3 m-2 text-lg w-1/2 text-center">
+                "Being passive gets you nowhere. Being aggressive gets you noticed. Being assertive
+                gets you respect." - Steve Maraboli
+              </p>
+            </div>
 
-        <div className="topic-of-day">
-          <p>Topic Of The Day: Syrup</p>
-        </div>
+            <div id="bottom-content" className="flex flex-col items-center justify-evenly">
+              <div id="topic-of-day" className="text-white text">
+                <p className="text-lg pb-4 mb-1">Topic Of The Day: Syrup</p>
+              </div>
 
-        {/* <div className="buttons">
-          
-        </div> */}
+              <div id="buttons" className="grid grid-cols-2 gap-2">
+                <Button text="My Profile" link="/" backgroundColor="primary" onClick={() => {}} />
+                <Button text="My Progress" link="/" backgroundColor="primary" onClick={() => {}} />
+                <Button text="New Session" link="/" backgroundColor="primary" onClick={() => {}} />
+                <Button text="My Settings" link="/" backgroundColor="primary" onClick={() => {}} />
+              </div>
+            </div>
+          </>
+        )}
       </main>
     </div>
   )
--- util/constants.ts
+++ util/constants.ts
@@ -1,6 +1,9 @@
 /* GENERAL */
 export const defaultTimer = '60'
 
-/* Assets */
+/* COLORS */
+export const primaryColor = '#449E90'
+
+/* ASSETS */
 export const assetServiceUrl = 'https://asset-service-drive.onrender.com'
 export const defaultAvatarImg = `${assetServiceUrl}/IMAGES/backgrounds/default-profile.png`
